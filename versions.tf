7fe8eba5a790cfdce5fe667626c9c133e23908fa
Sarah Hernandez <sarah.hernandez@hashicorp.com>
2022-06-07 15:39:26 -0700

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "hashicorp-learn"
+  # cloud {
+  #   organization = "sarah-test-org"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
